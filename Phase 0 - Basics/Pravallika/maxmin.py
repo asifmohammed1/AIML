5cd37c728ab64862dc292e60fd63b81f5f70d872
DADI PRAVALLIKA <144813209+Pravallikadadi@users.noreply.github.com>
2026-01-22 15:18:44 +0530

    minmax nums
    Added output comment for find_min_max function.

---
+++ Phase 0 - Basics/Pravallika/maxmin.py
@@ -0,0 +1,10 @@
+#Write a function that finds the largest and smallest number in a given list of numbers.
+def find_min_max(numbers):
+    if not numbers:
+        return None, None
+    return min(numbers), max(numbers)
+print(find_min_max([42.5, 17.3, 99.9, 3.14]))
+
+
+#output:(3.14, 99.9)
+   
