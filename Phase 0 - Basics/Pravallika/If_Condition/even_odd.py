38d5202efb8e94670437b1c55375fa12694b33cc
DADI PRAVALLIKA <144813209+Pravallikadadi@users.noreply.github.com>
2026-01-23 15:18:46 +0530

    Delete Phase 0 - Basics/Pravallika/If_Condition directory

--- Phase 0 - Basics/Pravallika/If_Condition/even_odd.py
+++
@@ -1,9 +0,0 @@
-#Write a program that accepts a number and checks whether it is even or odd.
-num = int(input("Enter an integer: "))
-if num % 2 == 0:
-    print(f"{num} is Even")
-else:
-    print(f"{num} is Odd")
-
-'''output:Enter an integer: 7
-   7 is Odd'''
