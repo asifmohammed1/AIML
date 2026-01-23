a4673c8353c4f30910b5854315d96af6bbc762d2
DADI PRAVALLIKA <144813209+Pravallikadadi@users.noreply.github.com>
2026-01-23 15:18:17 +0530

    Delete Phase 0 - Basics/Pravallika/For_Loops directory

--- Phase 0 - Basics/Pravallika/For_Loops/sum.py
+++
@@ -1,8 +0,0 @@
-#Use a for loop to iterate through a list of numbers and print the sum of all numbers.
-numbers = [7, 12, 4, 19, 8, 25]
-total = 0
-for n in numbers:
-    total += n
-print(f"Sum of {numbers} = {total}\n")
-
-#output:Sum of [7, 12, 4, 19, 8, 25] = 75
