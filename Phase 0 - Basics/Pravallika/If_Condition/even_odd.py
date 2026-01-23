255b977967cb8bddd761270f68dcc3c8d3354d1f
DADI PRAVALLIKA <144813209+Pravallikadadi@users.noreply.github.com>
2026-01-23 11:24:12 +0530

    even_odd

---
+++ Phase 0 - Basics/Pravallika/If_Condition/even_odd.py
@@ -0,0 +1,9 @@
+#Write a program that accepts a number and checks whether it is even or odd.
+num = int(input("Enter an integer: "))
+if num % 2 == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
+
+'''output:Enter an integer: 7
+   7 is Odd'''
