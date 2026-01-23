0db8de524bdbab9548d1c9be8571d592e0fb9b60
DADI PRAVALLIKA <144813209+Pravallikadadi@users.noreply.github.com>
2026-01-23 10:17:52 +0530

    Delete Phase 0 - Basics/Pravallika/strlen.py

--- Phase 0 - Basics/Pravallika/strlen.py
+++
@@ -1,6 +0,0 @@
-#Write a program that takes a list of strings and returns a new list with the lengths of each string.
-def get_string_lengths(strings):
-    return [len(s) for s in strings]
-print(get_string_lengths(["Pravallika", "Lallitha", "2026"]))
-
-#output:[10, 8, 4]
