b052ef181f7c712955d918b847fadcd75ab50899
Abhijeet Singh Gour <165776872+abhijeet013@users.noreply.github.com>
2026-01-27 15:31:38 +0530

    Add files via upload

---
+++ Phase 1 - Data, APIs & UI/Abhijeet/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class EmployeeCreate(BaseModel):
+    name: str
+    role: str
+    salary: int
+
+class EmployeeResponse(EmployeeCreate):
+    id: int
+
+    class Config:
+        from_attributes = True
+
